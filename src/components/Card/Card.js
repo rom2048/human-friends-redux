a9085299f9d515fe47a40a5c947363307680931b
Roman <mamontov.r1995@gmail.com>
2021-07-24 13:15:39 +0300

    change size img Card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({ id, name, email, phone, website }) => {
   return (
     <div className="tc grow bg-light-purple br2 shadow-3 ma3 pa3 br3 bw2 dib">
-      <img alt='human' src={`https://robohash.org/${id}?set=set5`} />
+      <img alt='human' src={`https://robohash.org/${id}?set=set5&size=150x150`} />
       <div>
         <h2>{name}</h2>
         <p>{email}</p>
